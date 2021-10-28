9229d1c86b71d19e90a5b7c79e08af65dce3d8c8
Nirmala Kuhl <nirmalakuhl@gmail.com>
2021-10-27 20:00:52 -0400

    db path

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/my-fam-db', {
-    useNewUrlParser: true,
+//mongoose.connect('mongodb://127.0.0.1:27017/my-fam-db', {
+mongoose.connect('mongodb+srv://myfam:T!hisismydatabasepassword789@cluster0.qphw2.mongodb.net/my-fam-db', {
+        useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
 })
