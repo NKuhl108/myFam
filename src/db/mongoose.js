a0e6b42892ba8a1b18e75622a10dafb91b5a4f37
Nirmala Kuhl <nirmalakuhl@gmail.com>
2021-10-27 20:11:02 -0400

    new db

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/my-fam-db', {
-//mongoose.connect('mongodb+srv://myfam:T!hisismydatabasepassword789@cluster0.qphw2.mongodb.net/my-fam-db', {
+//mongoose.connect('mongodb://127.0.0.1:27017/my-fam-db', {
+
+mongoose.connect('mongodb+srv://myfam:T!hisismydatabasepassword789@cluster0.qphw2.mongodb.net/my-fam-db2', {
         useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
