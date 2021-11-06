fde782084e0ab546a6027814959b5228c3339f4a
Nirmala Kuhl <nirmalakuhl@gmail.com>
2021-11-05 21:15:11 -0400

    send image link only for non-inmates

--- public/js/header.js
+++ public/js/header.js
@@ -1,1 +1,17 @@
-// here have a menu depending on who the person is
+// This little javascript file shows the link to send picture messages ONLY to non-inmates
+// (of course in addition we need to check on the backend)
+
+const imageLink = document.querySelector('#sendImageLink')
+
+
+const localstorage_user = JSON.parse(localStorage.getItem('user'))
+const inMemoryToken = localstorage_user.token
+console.log(localstorage_user.user.inmate)
+if (localstorage_user.user.inmate == false){
+   
+    imageLink.setAttribute('href', '/sendImage');
+    imageLink.innerHTML = 'Send Picture Message'
+}
+else{
+    imageLink.remove()
+}
